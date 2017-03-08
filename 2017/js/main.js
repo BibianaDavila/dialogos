ebcd91782128c8f0e45b99ef7ed7ce20d2150001
BibianaDavila <davila.bibiana@gmail.com>
2017-03-08 11:14:57 -0300

    Alterada identação

--- 2017/js/main.js
+++ 2017/js/main.js
@@ -5,20 +5,18 @@ $(document).ready(function(){
 
 	/* Largura pagina */
 	var docWidth = $(document).width();
-	console.log(docWidth);
 
 	if(docHeight>599 && docWidth>991){
 		$(".main").onepage_scroll({
-			sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
-			easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-			animationTime: 900, // AnimationTime let you define how long each section takes to animate
-		   	pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-		   	updateURL: true // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+		   sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
+		   easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+		   animationTime: 900, // AnimationTime let you define how long each section takes to animate
+		   pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
+		   updateURL: true // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 		});
 	}
 	
 });
-
 function init() {
     window.setTimeout(function(){
         start();
@@ -27,21 +25,12 @@ function init() {
 function start() {
 	$('body').removeClass("loading").addClass('loaded');
 }
-
 $(window).load(function() {
-	init(); // fade in page	
+	
+	init(); // fade in page
+	
 });
-
 $(window).resize(function() {
 
-	/* Altura pagina */
-	var docHeight = $(document).outerHeight();
-
-	/* Largura pagina */
-	var docWidth = $(document).width();
-
-	if(docHeight<600 || docWidth<992){
-		location.reload();
-	}
 
 });
